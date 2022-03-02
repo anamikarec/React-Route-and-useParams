134eeef01efeffe917fe9a977eeb80319080302e
Anamika Gupta <65801101+anamikarec@users.noreply.github.com>
2022-03-02 23:51:26 +0530

    with-switch-in-allroutes-and-default page

--- src/routes/AllRoutes.jsx
+++ src/routes/AllRoutes.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Route } from "react-router-dom";
+import { Route, Switch } from "react-router-dom";
 import { Navbar } from "../Components/Navbar";
 import { UserPage } from "../pages/UserPage";
 import { Users } from "../pages/Users";
@@ -8,24 +8,29 @@ const AllRoutes = () => {
   return (
     <>
       <Navbar />
-      <Route path="/" exact>
-        {" "}
-        <h3>Home Page</h3>{" "}
-      </Route>
+      <Switch>
+        <Route path="/" exact>
+          {" "}
+          <h3>Home Page</h3>{" "}
+        </Route>
 
-      <Route path="/about" exact>
-        {" "}
-        <h3>About Page</h3>{" "}
-      </Route>
-      <Route path="/users" exact>
-        {" "}
-        <Users />
-      </Route>
+        <Route path="/about" exact>
+          {" "}
+          <h3>About Page</h3>{" "}
+        </Route>
+        <Route path="/users" exact>
+          {" "}
+          <Users />
+        </Route>
 
-      <Route path="/users/:id" exact>
-        {" "}
-        <UserPage />
-      </Route>
+        <Route path="/users/:id" exact>
+          {" "}
+          <UserPage />
+        </Route>
+        <Route>
+          <div>Page not found</div>
+        </Route>
+      </Switch>
     </>
   );
 };
